5763b51b1322e6db1d7038ea3123dcc81b07cf52
jaxon grosam <jaxon.grosam@gmail.com>
2023-09-04 18:37:50 -0500

    begin inquirer prompt

--- index.js
+++ index.js
@@ -7,3 +7,30 @@ function init() {
   const logoText = logo({ name: "Employee Manager" }).render();
   console.log(logoText);
 }
+
+async function menu() {
+  const answer = await inquirer.createPromptModule([
+    {
+      type: "list",
+      name: "menu",
+      message: "What would you like to do?",
+      choices: [
+        "View all employees",
+        "View all roles",
+        "View all departments",
+        "Add employee",
+        "Add role",
+        "Add department",
+        "Update employee role",
+        "Update an employee manager",
+        "View employees by manager",
+        "View employees by department",
+        "Delete an employee",
+        "Delete a role",
+        "Delete a department",
+        "View budget",
+        "Quit",
+      ],
+    },
+  ]);
+}
